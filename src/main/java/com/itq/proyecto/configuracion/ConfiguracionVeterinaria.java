e14a99872ddf3aa680a2c35beb8ceec96ea7dbb3
Juan Sebastian Alzate Leon <ing.jsalzateleon@gmail.com>
2023-09-27 21:20:17 -0600

    creacion cita medica

--- src/main/java/com/itq/proyecto/configuracion/ConfiguracionVeterinaria.java
+++ src/main/java/com/itq/proyecto/configuracion/ConfiguracionVeterinaria.java
@@ -3,9 +3,12 @@ package com.itq.proyecto.configuracion;
 import com.itq.proyecto.repositorio.RepositorioUsuario;
 import com.itq.proyecto.servicio.CreacionUsuarioServicio;
 import com.itq.proyecto.servicio.impl.CreacionUsuarioServicioImpl;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class ConfiguracionVeterinaria {
@@ -15,4 +18,5 @@ public class ConfiguracionVeterinaria {
     public CreacionUsuarioServicio crearUsuarioServicio(RepositorioUsuario repositorioUsuario){
         return new CreacionUsuarioServicioImpl(repositorioUsuario);
     }
+
 }
